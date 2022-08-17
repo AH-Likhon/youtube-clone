488a5aaa7e31ee58ea3357fbf8abcf6f70233861
plcpl-haque <md.haque@profecialinks.com>
2022-08-17 14:52:45 +0600

    time to deploy

--- server/index.js
+++ server/index.js
@@ -6,11 +6,21 @@ import videoRouter from './routers/videos.js';
 import commentRouter from './routers/comments.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
+// const cors = require('cors');
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+
+// <-----------------------------  cors option ----------------------------------> //
+// const corsOptions = {
+//     // origin: 'http://localhost:3000',
+//     origin: 'https://real-time-chat-web-app.netlify.app',
+//     credentials: true,
+//     optionSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 
 const connectDB = () => {
     mongoose.connect(process.env.DB).then(() => {
