b63dee98d8a1156cc851579e99ba37ed90b23b29
AH-Likhon <ictianlikhon6@gmail.com>
2023-09-30 16:52:54 +0600

    update server packages

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -4,141 +4,145 @@ import User from "../models/User.js";
 
 // ------------------------------------ ADD VIDEO FILE --------------------------- //
 export const addVideo = async (req, res, next) => {
-    const newVideo = new Video({ userId: req.user.id, ...req.body });
-    try {
-        const savedVideo = await newVideo.save();
-        res.status(200).json(savedVideo);
-    } catch (error) {
-        next(error);
-    }
+  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // --------------------------------- UPDATE VIDEO FILE --------------------------- //
 export const updateVideo = async (req, res, next) => {
-    try {
-        const video = await Video.findById(req.params.id);
-        if (!video) {
-            return next(createError(404, 'Video Not Found!'));
-        };
-        if (req.user.id === video.userId) {
-            const updatedVideo = await Video.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-            res.status(200).json(updatedVideo);
-        } else {
-            return next(createError(403, 'You can update only your video!'));
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, "Video Not Found!"));
     }
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(createError(403, "You can update only your video!"));
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 // --------------------------------- DELETE VIDEO FILE --------------------------- //
 export const deleteVideo = async (req, res, next) => {
-    try {
-        const video = await Video.findById(req.params.id);
-        if (!video) {
-            return next(createError(404, 'Video Not Found!'));
-        };
-        if (req.user.id === video.userId) {
-            await Video.findByIdAndDelete(req.params.id);
-            res.status(200).json('The video has been deleted successfully!');
-        } else {
-            return next(createError(403, 'You can delete only your video!'));
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, "Video Not Found!"));
     }
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("The video has been deleted successfully!");
+    } else {
+      return next(createError(403, "You can delete only your video!"));
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ------------------------------------ GET VIDEO FILE --------------------------- //
 export const getVideo = async (req, res, next) => {
-    try {
-        const video = await Video.findById(req.params.id);
-        res.status(200).json(video);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const video = await Video.findById(req.params.id);
+    res.status(200).json(video);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ---------------------------------- GET MY VIDEOS FILE --------------------------- //
 export const getMyVideos = async (req, res, next) => {
-    console.log(req.params.userId);
-    try {
-        const videos = await Video.find({ userId: req.params.userId });
-        res.status(200).json(videos);
-    } catch (error) {
-        next(error);
-    }
+  // console.log(req.params.userId);
+  try {
+    const videos = await Video.find({ userId: req.params.userId });
+    res.status(200).json(videos);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ------------------------------------- ADD VIEW FILE --------------------------- //
 export const addView = async (req, res, next) => {
-    try {
-        await Video.findByIdAndUpdate(req.params.id, { $inc: { views: 1 } });
-        res.status(200).json('The view has been successfully increased!');
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await Video.findByIdAndUpdate(req.params.id, { $inc: { views: 1 } });
+    res.status(200).json("The view has been successfully increased!");
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ---------------------------------- TRENDING VIEW FILE ------------------------- //
 export const trend = async (req, res, next) => {
-    try {
-        const videos = await Video.find().sort({ views: -1 });
-        res.status(200).json(videos);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const videos = await Video.find().sort({ views: -1 });
+    res.status(200).json(videos);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ----------------------------------- RANDOM VIEW FILE -------------------------- //
 export const random = async (req, res, next) => {
-    try {
-        const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
-        res.status(200).json(videos);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+    res.status(200).json(videos);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ------------------------------------ SUBSCRIBED FILE -------------------------- //
 export const sub = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.user.id);
-        const subscribedChannels = user.subscribedUsers;
+  try {
+    const user = await User.findById(req.user.id);
+    const subscribedChannels = user.subscribedUsers;
 
-        const list = await Promise.all(
-            subscribedChannels.map(channelId => {
-                return Video.find({ userId: channelId });
-            })
-        );
+    const list = await Promise.all(
+      subscribedChannels.map((channelId) => {
+        return Video.find({ userId: channelId });
+      })
+    );
 
-        res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt));
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt));
+  } catch (error) {
+    next(error);
+  }
 };
 
 // --------------------------------------- GET BY TAG ---------------------------- //
 export const getByTags = async (req, res, next) => {
-    const tags = req.query.tags.split(',');
-    // console.log(tags);
-    try {
-        const videos = await Video.find({ tags: { $in: tags } }).limit(20);
-        res.status(200).json(videos);
-    } catch (error) {
-        next(error);
-    }
+  const tags = req.query.tags.split(",");
+  // console.log(tags);
+  try {
+    const videos = await Video.find({ tags: { $in: tags } }).limit(20);
+    res.status(200).json(videos);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // ------------------------------------------- Search ---------------------------- //
 export const search = async (req, res, next) => {
-    const query = req.query.q;
-    try {
-        const videos = await Video.find({
-            title: { $regex: query, $options: "i" }
-        }).limit(40);
-        res.status(200).json(videos);
-    } catch (error) {
-        next(error);
-    }
+  const query = req.query.q;
+  try {
+    const videos = await Video.find({
+      title: { $regex: query, $options: "i" },
+    }).limit(40);
+    res.status(200).json(videos);
+  } catch (error) {
+    next(error);
+  }
 };
--- server/index.js
+++ server/index.js
@@ -1,67 +1,70 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import userRouter from './routers/users.js';
-import videoRouter from './routers/videos.js';
-import commentRouter from './routers/comments.js';
-import authRouter from './routers/auth.js';
-
+import userRouter from "./routers/users.js";
+import videoRouter from "./routers/videos.js";
+import commentRouter from "./routers/comments.js";
+import authRouter from "./routers/auth.js";
 
 const app = express();
 dotenv.config();
 
 const connectDB = () => {
-    mongoose.connect(process.env.DB).then(() => {
-        console.log("Connected with database");
-    }).catch((error) => {
-        throw error;
+  mongoose
+    .connect(process.env.DB)
+    .then(() => {
+      // console.log("Connected with database");
+    })
+    .catch((error) => {
+      throw error;
     });
 };
 
-
 // <-----------------------------  cors option ----------------------------------> //
 const corsOptions = {
-    origin: 'https://fir-21b25.firebaseapp.com',
-    // origin: 'http://localhost:3000',
-    // origin: 'https://fir-21b25.web.app',
-    preflightContinue: true,
-    credentials: true,
-    optionSuccessStatus: 200
+  origin: "https://fir-21b25.firebaseapp.com",
+  // origin: 'http://localhost:3000',
+  // origin: 'https://fir-21b25.web.app',
+  // preflightContinue: true,
+  // credentials: true,
+  // optionSuccessStatus: 200
 };
 // origin: 'https://fir-21b25.firebaseapp.com',
 
 app.use(cors(corsOptions));
+app.options("*", cors());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 // app.use(bodyParser.json({ limit: "50mb" }));
 // app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
-app.use('/api/videos', videoRouter);
-app.use('/api/comments', commentRouter);
-
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/videos", videoRouter);
+app.use("/api/comments", commentRouter);
 
 // handle error middleware
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || 'Something went wrong';
-    return res.status(status).json({
-        success: false,
-        status,
-        message
-    });
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
 
-app.get('/', (req, res) => {
-    res.send('ok');
+app.get("/", (req, res) => {
+  res.send("ok");
 });
 
 app.listen(process.env.PORT || 5000, () => {
-    connectDB();
-    console.log('Connected......');
-})
+  connectDB();
+  // console.log('Connected......');
+});
