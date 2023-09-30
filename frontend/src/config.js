5f4aef1c893e3fa750e100d0c2cbd9da015c060b
AH-Likhon <ictianlikhon6@gmail.com>
2023-09-30 17:31:43 +0600

    remove vercel.json

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://youtube-server-f7jy.onrender.com/api",
-  withCredentials: true
-})
+  withCredentials: true,
+});
 
-  // baseURL: "https://youtube-server-f7jy.onrender.com/api"
-  // http://localhost:5000
+// baseURL: "https://youtube-server-api.vercel.app/api"
+// baseURL: "https://youtube-server-f7jy.onrender.com/api"
+// http://localhost:5000
